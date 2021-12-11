d6e8117d0d82f2a2c709d32bcfec00968c6e1071
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-12-11 20:18:15 +0530

    io sockets

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io  = require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
@@ -33,11 +35,15 @@ app.post('/messages',(req,res)=>
         console.log("error")
     }
     console.log(req.body)
-    
+    io.emit('message',req.body)
     res.sendStatus(200)
 })
 
-app.listen(server,()=>
+io.on('connection',(socket)=>{
+    console.log('a user connected');
+})
+
+http.listen(server,()=>
 {
     console.log("Server running on: ",server)
 })
