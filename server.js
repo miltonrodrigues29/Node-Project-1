37d4854e482ff72f9f7b408c82607a1b0cbdb308
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-12-13 13:55:32 +0530

    -

--- server.js
+++ server.js
@@ -31,31 +31,63 @@ app.get('/messages',(req,res)=>
 
 })
 
-app.post('/messages',(req,res)=>
+app.post('/messages',async (req,res)=>
 {
-    var message = new Message(req.body)
-    message.save()
-    .then(()=>
-    {
-        console.log('saved')
-        return Message.findOne({message:'badword'})
-    })
-    .then( censored =>{
-        if(censored)
-        {
-            console.log('Censored words found',censored)
-            return Message.deleteOne({_id:censored.id})
-        }
+    try{
+    
+       
+        var message = new Message(req.body)
+
+    var savedMessage = await message.save()
+
+    console.log('saved')
+
+    var censored = await Message.findOne({message:'bad word'})
+
+    if (censored)
+        await Message.deleteMany({_id: censored.id})
+    else 
         io.emit('message',req.body)
         res.sendStatus(200)
-    }).catch((err)=>
-    {
+
+    }
+    catch(error){
         res.sendStatus(500)
-        return console.error(err)
-    })
+        return console.error(error)
+    }
+    finally
+    {
+        console.log("Finally block called");
+    }
+    
+})
+
+
+// app.post('/messages',(req,res)=>
+// {
+//     var message = new Message(req.body)
+//     message.save()
+//     .then(()=>
+//     {
+//         console.log('saved')
+//         return Message.findOne({message:'badword'})
+//     })
+//     .then( censored =>{
+//         if(censored)
+//         {
+//             console.log('Censored words found',censored)
+//             return Message.deleteOne({_id:censored.id})
+//         }
+//         io.emit('message',req.body)
+//         res.sendStatus(200)
+//     }).catch((err)=>
+//     {
+//         res.sendStatus(500)
+//         return console.error(err)
+//     })
     
 
-})
+// })
 // app.post('/messages',(req,res)=>
 // {   
 //     var message = new Message(req.body)
